a919e5cbfe324995b4d5c85ad8097efe486a2d29
Mudit Kumar <muditksj6@gmail.com>
2026-07-13 16:30:57 +0530

    err

--- Database/db.js
+++ Database/db.js
@@ -3,15 +3,16 @@ let mongoose = require("mongoose");
 let dotenv = require("dotenv");
 dotenv.config();
 
-let mongo_db_url = process.env.mongo_db_url;
+// let mongo_db_url = process.env.mongo_db_url;
+console.log(process.env.mongo_db_url);
 
 mongoose
-  .connect(mongo_db_url)
+  .connect(process.env.mongo_db_url)
   .then(() => {
     console.log("Database connected successfully");
   })
   .catch((err) => {
-    console.log("Database not connected");
+    console.log("Database not connected>......");
     console.log(err);
   });
 
